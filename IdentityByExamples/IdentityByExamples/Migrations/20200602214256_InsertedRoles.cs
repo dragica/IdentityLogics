04583a5cc1d58277197c749e1dfb8d0e99f3bd9f
Dragica <dragican@live.com>
2020-06-05 21:01:45 +0200

    Project files added

---
+++ IdentityByExamples/IdentityByExamples/Migrations/20200602214256_InsertedRoles.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IdentityByExamples.Migrations
+{
+    public partial class InsertedRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "5ff626cf-a107-4d45-8b97-aeba1e4a8e28", "0c199422-8770-4d9a-b024-e928d86c6e68", "Visitor", "VISITOR" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "1f1b4192-484a-44e5-9987-385ec54fec39", "9b0c7ef4-93f5-4242-af71-91fbb60ec59d", "Administrator", "ADMINISTRATOR" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1f1b4192-484a-44e5-9987-385ec54fec39");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "5ff626cf-a107-4d45-8b97-aeba1e4a8e28");
+        }
+    }
+}
